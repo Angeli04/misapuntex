7db85e19c390661f2c7e625fdfa68003361af89c
Angeli04 <angeliorozco@sanluis.edu.ar>
2022-11-23 19:21:55 -0300

    detalles en formularios

---
+++ forms.js
@@ -0,0 +1,54 @@
+function validar(){
+    let nombre = document.getElementById("nombre");
+    let apellido = document.getElementById("apellido");
+    let mail = document.getElementById("mail");
+    var telefono=document.getElementById("telefono");
+ 
+    let lista = document.getElementById("listaErrores")
+    let listaM = document.getElementById("listaMensajes")
+    let flag =true;
+ 
+ 
+    lista.innerHTML = "";
+    nombre.classList.remove("error");
+    mail.classList.remove("error");
+    apellido.classList.remove("error");
+    telefono.classList.remove("error");
+    if (nombre.value=="") {
+        console.log("Nombre invalido");
+        let item = document.createElement("li");
+        lista.appendChild(item);
+        item.innerHTML="Nombre invalido";
+        nombre.classList.add("error");
+        flag=false;
+    }
+    if (apellido.value=="") {
+       console.log("Apellido invalido");
+       let item = document.createElement("li");
+       lista.appendChild(item);
+       item.innerHTML="Apellido invalido";
+       apellido.classList.add("error");
+       flag=false;
+    }
+    if(mail.value==""){
+       console.log("Mail invalido");
+       let item = document.createElement("li");
+       lista.appendChild(item);
+       item.innerHTML="Mail invalido";
+       mail.classList.add("error");
+       flag=false;
+    }
+    if (telefono.value==""||isNaN(telefono.value)) {
+       console.log("Telefono invalido");
+       let item = document.createElement("li");
+       lista.appendChild(item);
+       item.innerHTML= "Telefono invalido";
+       telefono.classList.add("error");
+       flag = false;
+    }
+    let item = document.createElement("li");
+    lista.appendChild(item);
+    item.innerHTML= "Formulario enviado correctamente";
+    console.log("Todo ok!");
+    return false;
+ }
